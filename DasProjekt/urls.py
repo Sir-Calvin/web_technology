9a6a85e626c7bf060a494d1bacbc1d45a48495e3
Sir-Calvin <schiggime@gmail.com>
2024-10-22 13:05:53 +0000

    Fix DEBUG setting and add static URL configuration

--- DasProjekt/urls.py
+++ DasProjekt/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ersteapp.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# Nur wenn DEBUG = True, d.h. in der Entwicklungsumgebung
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
