3f0edc9137326d644c29737cc7978a61395b922a
Sir-Calvin <schiggime@gmail.com>
2024-10-22 08:35:44 +0000

    Fixen der Fehler die durch den letzten Commit entstanden sind. (Views die nicht genutzt waren, static nicht in den HTML Dateien integriert)

--- DasProjekt/settings.py
+++ DasProjekt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9+&r*pvne^q46-$5c67(k=d%3drj50s+(1lx$5kkq+k34x#3k)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.0.104', '127.0.0.1', 'localhost', '80.158.79.158']
 
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
